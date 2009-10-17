8440a42201e7dbfc848a391299c5e6c33b1b98ff
U-LAKEOZ\Robert <Robert@LAKEOZ.(none)>
2009-10-16 20:47:46 -0700

    Move viewwindow to Viewer. Not enabled yet

---
+++ src/LookingGlass.View/ViewWindow.Designer.cs
@@ -0,0 +1,95 @@
+﻿/* Copyright (c) Robert Adams
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * The name of the copyright holder may not be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+namespace LookingGlass.View {
+    partial class ViewWindow {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Override class that wraps Panel and makes sure it doesn't get
+        /// painted so the OnPaint operation only is the callback taht we
+        /// registered in the regular RadegastWindow class.
+        /// </summary>
+        private class DBPanel : System.Windows.Forms.Panel {
+            public DBPanel()
+                : base() {
+                // DoubleBuffered = true;
+                SetStyle(System.Windows.Forms.ControlStyles.AllPaintingInWmPaint, true);
+                SetStyle(System.Windows.Forms.ControlStyles.UserPaint, true);
+                // SetStyle(System.Windows.Forms.ControlStyles.Opaque, true);
+            }
+            protected override void OnPaintBackground(System.Windows.Forms.PaintEventArgs e) {
+                // base.OnPaintBackground(e);
+            }
+            
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            // this.renderingPanel = new System.Windows.Forms.Panel();
+            this.renderingPanel = new DBPanel();
+            this.SuspendLayout();
+            // 
+            // renderingPanel
+            // 
+            this.renderingPanel.Location = new System.Drawing.Point(5, 5);
+            this.renderingPanel.Name = "renderingPanel";
+            this.renderingPanel.Size = new System.Drawing.Size(800, 600);
+            this.renderingPanel.TabIndex = 0;
+            // 
+            // ViewWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 608);
+            this.Controls.Add(this.renderingPanel);
+            this.Name = "ViewWindow";
+            this.Text = "ViewWindow";
+            this.Load += new System.EventHandler(this.ViewWindow_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel renderingPanel;
+    }
+}
--- src/LookingGlass.View/ViewWindow.cs
+++ src/LookingGlass.View/ViewWindow.cs
@@ -28,27 +28,23 @@ using System.Drawing;
 using System.Text;
 using System.Threading;
 using System.Windows.Forms;
-using Radegast;
 using LookingGlass;
 using LookingGlass.Framework.Logging;
 using LookingGlass.Renderer;
 
-namespace LookingGlass.Radegast {
+namespace LookingGlass.View {
+public partial class ViewWindow : Form {
 
-public partial class RadegastWindow : Form {
-
-    private RadegastInstance m_radInstance;
     private LookingGlassBase m_lgb;
     private Panel m_renderPanel;
     private IRenderProvider m_renderer;
     private System.Threading.Timer m_refreshTimer;
+    private int m_framesPerSec;
 
     private PaintEventHandler m_paintEventHandler = null;
     private EventHandler m_resizeEventHandler = null;
 
-    // Called before LookingGlass is initialized
-    public RadegastWindow(RadegastInstance rinst, LookingGlassBase lgbase) {
-        m_radInstance = rinst;
+    public ViewWindow(LookingGlassBase lgbase) {
         m_lgb = lgbase;
 
         InitializeComponent();
@@ -57,22 +53,32 @@ public partial class RadegastWindow : Form {
     // Called after LookingGlass is initialized
     public void Initialize() {
         // get a handle to the renderer module in LookingGlass
-        string rendererName = m_lgb.AppParams.ParamString("Radegast.Renderer.Name");
+        string rendererName = m_lgb.AppParams.ParamString("Viewer.Renderer.Name");
+        m_framesPerSec = m_lgb.AppParams.ParamInt("Viewer.FramesPerSec");
         m_renderer = (IRenderProvider)m_lgb.ModManager.Module(rendererName);
 
-        Control[] subControls = this.Controls.Find("LGWindow", true);
+        Control[] subControls = this.Controls.Find("viewPanel", true);
         if (subControls.Length == 1) {
             m_renderPanel = (Panel)subControls[0];
         }
-        m_paintEventHandler = new System.Windows.Forms.PaintEventHandler(radControl_Paint);
+        m_paintEventHandler = new System.Windows.Forms.PaintEventHandler(viewControl_Paint);
         m_renderPanel.Paint += m_paintEventHandler;
-        m_resizeEventHandler = new System.EventHandler(radControl_Resize);
+        m_resizeEventHandler = new System.EventHandler(viewControl_Resize);
         m_renderPanel.Resize += m_resizeEventHandler;
+        // m_renderPanel.MouseClick
+        // m_renderPanel.MouseWheel
+        // m_renderPanel.MouseMove
+        // m_renderPanel.MouseLeave
+        // m_renderPanel.MouseDown
+        // m_renderPanel.MouseUp
 
         m_refreshTimer = new System.Threading.Timer(delegate(Object param) {
             m_renderPanel.Invalidate();
         }, null, 2000, 100);
 
+    }
+    
+    private void ViewWindow_Load(object sender, EventArgs e) {
     }
 
     public void Shutdown() {
@@ -88,7 +94,7 @@ public partial class RadegastWindow : Form {
         if (m_resizeEventHandler != null) m_renderPanel.Resize -= m_resizeEventHandler;
     }
 
-    public void radControl_Paint(object sender, PaintEventArgs e) {
+    public void viewControl_Paint(object sender, PaintEventArgs e) {
         if (this.InvokeRequired) {
             BeginInvoke((MethodInvoker)delegate() { m_renderer.RenderOneFrame(false, 100); });
         }
@@ -98,7 +104,7 @@ public partial class RadegastWindow : Form {
         return;
     }
 
-    public void radControl_Resize(object sender, EventArgs e) {
+    public void viewControl_Resize(object sender, EventArgs e) {
         return;
     }
 }
